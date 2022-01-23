82b7b0a5b9edf9a6fcfba58e7e8b7257e63149f2
hoanghip2632002 <hoanghip2632002@gmail.com>
2022-01-23 11:38:20 +0700

    fdsfsd

--- src/components/admin/navbarAdmin.js
+++ src/components/admin/navbarAdmin.js
@@ -50,7 +50,7 @@ const navbar_admin = {
           <div class="user-avatar">
             <img src="https://avatars.dicebear.com/v2/initials/john-doe.svg" alt="John Doe" class="rounded-full">
           </div>
-          <div class="is-user-name"><span>Vanthanh</span></div>
+          <div class="is-user-name"><span>PH17225</span></div>
           <span class="icon"><i class="mdi mdi-chevron-down"></i></span>
         </a>
       
